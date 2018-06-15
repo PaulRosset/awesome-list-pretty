0747d0badf54b7290072a881e62c694d025a9a34
Paul Rosset <paulrosset96@gmail.com>
2018-06-16 00:43:15 +0100

    fix.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <InstantSearch
-          appId="W4TTTJ58U4"
-          apiKey="f4e9e1d0bfe2d4348a852e23409d814c"
-          indexName="dev_AWESOME"
-        >
+        <InstantSearch appId="" apiKey="" indexName="">
           <Header />
           <WrappedBody />
           <Authors />
